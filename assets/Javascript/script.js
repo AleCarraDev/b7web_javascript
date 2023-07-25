5dccf7c15fa7c1a90f9eded93f61782947b3a8cd
AlessandroGrupoGera <22fimdetarde@gmail.com>
2023-07-25 08:36:53 -0300

    aula 6 tipos de dados

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,6 +1,16 @@
-let nome = "Alessandro"
-let idade = 31
-let cidade = "Rio de janeiro"
-let estado = "Rio de janeiro"
+// let nome = "Alessandro"
+// let idade = 31
+// let cidade = "Rio de janeiro"
+// let estado = "Rio de janeiro"
 
-console.log( cidade + " " + estado )
+// console.log( cidade + " " + estado )
+
+// tipos de dados aula 6
+
+let nome = "Alessandro" // Esse dado chama-se "string"
+let idade = 31 // esse dado chama-se "number"
+let deslogado = false // esse dado é um booleano
+let lista = ["arroz", "feijão", "carne", "vegetais"] // esse dado é um array
+let nomeCompleto = {nome: "Alessandro", idade: 31} // esse dado é um objeto
+
+console.log(typeof nomeCompleto)
