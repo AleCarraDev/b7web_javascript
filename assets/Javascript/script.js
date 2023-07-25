85a22a59e6d7911622518ce4ffca79ecc8152e54
AlessandroGrupoGera <22fimdetarde@gmail.com>
2023-07-25 08:52:36 -0300

    utilizando ; para finalizar trecho de codigo

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,17 +1,22 @@
-// 1. Crie uma variável para armazenar o nome
-// do carro e ponha o valor Ferrari. E exiba-a
-let carro = "Ferrari"
+// // 1. Crie uma variável para armazenar o nome
+// // do carro e ponha o valor Ferrari. E exiba-a
+// let carro = "Ferrari"
 
-// 2. Crie uma variável para armazenar o preço do bolo,
-// que custa R$19,99. E exiba-a
- let valorBolo = 19.99 // "R$ 19,99" utilizei como variável pois quis exibir o cifrão
-
-
-// 3. Corrija o código a segue (e exiba-o).
-// let "cidade" = São Paulo
-let cidade = "São Paulo"
+// // 2. Crie uma variável para armazenar o preço do bolo,
+// // que custa R$19,99. E exiba-a
+//  let valorBolo = 19.99 // "R$ 19,99" utilizei como variável pois quis exibir o cifrão
 
 
-console.log(carro)
-console.log(valorBolo)
-console.log(cidade)
+// // 3. Corrija o código a segue (e exiba-o).
+// // let "cidade" = São Paulo
+// let cidade = "São Paulo"
+
+
+// console.log(carro)
+// console.log(valorBolo)
+// console.log(cidade)
+
+let nome = "Alessandro"; let idade = "31";
+
+console.log(nome);
+console.log(idade);
