7a6c7850f884aa35b03fe595f0d27bc1b599cd96
AlessandroGrupoGera <22fimdetarde@gmail.com>
2023-07-25 08:39:15 -0300

    utilizando comentario em codigo aula 7

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -7,10 +7,10 @@
 
 // tipos de dados aula 6
 
-let nome = "Alessandro" // Esse dado chama-se "string"
-let idade = 31 // esse dado chama-se "number"
-let deslogado = false // esse dado é um booleano
-let lista = ["arroz", "feijão", "carne", "vegetais"] // esse dado é um array
-let nomeCompleto = {nome: "Alessandro", idade: 31} // esse dado é um objeto
+// let nome = "Alessandro" // Esse dado chama-se "string"
+// let idade = 31 // esse dado chama-se "number"
+// let deslogado = false // esse dado é um booleano
+// let lista = ["arroz", "feijão", "carne", "vegetais"] // esse dado é um array
+// let nomeCompleto = {nome: "Alessandro", idade: 31} // esse dado é um objeto
 
-console.log(typeof nomeCompleto)
+// console.log(typeof nomeCompleto)
